e1449c64daf952ff6a35655a2d273087c3446662
AmirAlahmedy <ameer.alahmedy@gmail.com>
2026-08-28 15:54:18 +0300

    feat: add swagger

--- backend/inventory/inventory-app/src/main/java/com/amir/inventory/reactive/controller/ProductController.java
+++ backend/inventory/inventory-app/src/main/java/com/amir/inventory/reactive/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.amir.inventory.reactive.controller;
 
 import com.amir.inventory.domain.Product;
 import com.amir.inventory.reactive.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,8 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
+@Tag(name = "Product Management", description = "APIs for managing consumer products")
 public class ProductController {
 
     private final ProductService productService;
@@ -20,18 +24,25 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/products")
+    @GetMapping("/")
+    @Operation(summary = "List all products", description = "List all products in the inventory.")
+    @ApiResponse(responseCode = "200", description = "Products successfully retrieved")
     public Flux<Product> getAllProducts() {
         return productService.getProducts();
     }
 
-    @PostMapping("/products")
+    @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a product", description = "Creates a product using the details sent in the body.")
+    @ApiResponse(responseCode = "201", description = "Product successfully created")
     public Mono<Product> createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
+    @Operation(summary = "Get product by ID", description = "Fetches details of a product using its unique identifier.")
+    @ApiResponse(responseCode = "200", description = "Product successfully found")
+    @ApiResponse(responseCode = "404", description = "Product not found")
     public Mono<ResponseEntity<Product>> getProduct(@PathVariable String id) {
         return productService.findById(id)
                 .map(product -> ResponseEntity.ok(product))
